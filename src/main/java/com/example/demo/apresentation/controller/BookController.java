9ff52b29d514cb8e3a6d17e4de71f36f815372e2
jvomamede <joaovitoroliveiramamede446755@gmail.com>
2023-10-15 16:22:28 -0300

    Add Post Method

--- src/main/java/com/example/demo/apresentation/controller/BookController.java
+++ src/main/java/com/example/demo/apresentation/controller/BookController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.infra.service.BookService;
 
@@ -17,6 +19,12 @@ public class BookController {
     public String books(Model model) {
         String[] books = this.bookService.books();
         model.addAttribute("books", books);
+        return "books";
+    }
+
+    @PostMapping("/books/create")
+    public String createBook(@RequestParam(name = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
         return "books";
     }
 
